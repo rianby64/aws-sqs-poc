d1acc94726e3c69d6114fc23f63b230a14843a3b
Andres Rios <rianby64@gmail.com>
2021-01-23 16:33:07 +0300

    Test the listen method when it receives two messages

--- queue/listen_test.go
+++ queue/listen_test.go
@@ -4,14 +4,16 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/stretchr/testify/assert"
 )
 
 type Mock4ReceiveMessageAWSSession struct {
-	CalledReceiveMessage bool
-	Locker               sync.Mutex
-	Waiter               sync.WaitGroup
+	CalledReceiveMessage    bool
+	Locker                  sync.Mutex
+	Waiter                  sync.WaitGroup
+	ReceiveMessageResponses []*sqs.Message
 }
 
 func (a *Mock4ReceiveMessageAWSSession) SendMessage(input *sqs.SendMessageInput) (*sqs.SendMessageOutput, error) {
@@ -24,7 +26,7 @@ func (a *Mock4ReceiveMessageAWSSession) ReceiveMessage(input *sqs.ReceiveMessage
 	a.Waiter.Done()
 
 	response := sqs.ReceiveMessageOutput{
-		Messages: []*sqs.Message{},
+		Messages: a.ReceiveMessageResponses,
 	}
 
 	return &response, nil
@@ -38,7 +40,9 @@ func (a *Mock4ReceiveMessageAWSSession) DeleteMessage(input *sqs.DeleteMessageIn
 	Case 1: queue.listen(handler) calls ReceiveMessage at least once
 */
 func Test_listen_calls_ReceiveMessage(t *testing.T) {
-	session := &Mock4ReceiveMessageAWSSession{}
+	session := &Mock4ReceiveMessageAWSSession{
+		ReceiveMessageResponses: []*sqs.Message{},
+	}
 	session.Waiter.Add(1) // wait for the call of ReceiveMessage
 
 	handler := func(str string) error {
@@ -53,4 +57,43 @@ func Test_listen_calls_ReceiveMessage(t *testing.T) {
 	session.Waiter.Wait()
 
 	assert.True(t, session.CalledReceiveMessage)
+}
+
+/*
+	Case 1: queue.listen(handler) calls ReceiveMessage at least once
+*/
+func Test_listen_calls_ReceiveMessage_with_two_responses(t *testing.T) {
+	msg1 := "message 1"
+	msg2 := "message 2"
+	session := &Mock4ReceiveMessageAWSSession{
+		ReceiveMessageResponses: []*sqs.Message{
+			{
+				Body: aws.String(msg1),
+			},
+			{
+				Body: aws.String(msg2),
+			},
+		},
+	}
+	session.Waiter.Add(3) // wait for the call of ReceiveMessage and the handler twice
+
+	i := 0
+	handler := func(str string) error {
+		if msg1 == str || msg2 == str {
+			i++
+			session.Waiter.Done()
+		}
+
+		return nil
+	}
+
+	queue := queueSQS{
+		SQS: session,
+	}
+
+	go queue.listen(handler)
+	session.Waiter.Wait()
+
+	assert.True(t, session.CalledReceiveMessage)
+	assert.Equal(t, 2, i)
 }
